39480cae6ae987da62f09e5b69a046b7f7b11a26
Dipesh Bajgain <dipbazz@gmail.com>
2021-05-04 15:58:40 +0545

    Refactor app component to be stateful
    default total, next, and operation state added in constructor
    
    passed the property of handleClick to ButtonPanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
+import React from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-calculate({ total: '10', next: '5', operation: 'X' }, '=');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '',
+      next: '',
+      operation: null,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+
+    this.setState(calculate({ total, next, operation }, buttonName));
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <div className="App">
+        <Display result={total} />
+        <ButtonPanel handleClick={(name) => this.handleClick(name)} />
+      </div>
+    );
+  }
 }
 
 export default App;
